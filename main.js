d31a08c6d8dfbb4bbb75bed267bb31358e2aa9ef
thaufe <toni.haufe@softwareag.com>
2019-11-15 16:58:32 +0100

    Save CSV file to user directory

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const { app, BrowserWindow } = require('electron');
 const timer = require('./timer.js');
 const Project = require('./project.js');
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
 
 let win;
-const projects = {};
+const homeDir = os.homedir();
+const projects = [];
 let currentProjectId = null;
 
 const createWindow = function createWindow () {
@@ -18,11 +22,36 @@ const createWindow = function createWindow () {
   win.loadFile('index.html');
   timer.start = Date.now();
   let defaultProject = new Project(0, 'Orga', 'RnD Emails, OpenAir, iTrac');
-  projects[defaultProject.id] = defaultProject;
+  projects.push(defaultProject);
   currentProjectId = defaultProject.id;
   win.on('closed', () => {
     win = null;
   });
+}
+
+const saveReport = function saveReport(projects) {
+  const dir = path.join(homeDir, 'ts');
+  const yyyymmdd =  (new Date().toISOString().split('T'))[0];
+  const fileName = 'TimeSplit_' + yyyymmdd + '.csv';
+  const csvString = createCsvString(projects);
+  writeFile(dir, fileName, csvString);
+};
+
+const createCsvString = function createCsvString(projects) {
+  let csv = '';
+  for (let i = 0; i < projects.length; i++) {
+    csv += (i === 0) ? '' : '\n';
+    csv += projects[i].toCsvRecord(); 
+  }
+  return csv;
+}
+
+const writeFile = function writeFile(directory, fileName, content) {
+  // console.log('Writing file to ' + path.join(directory, fileName));
+  if (!fs.existsSync(directory)) {
+    fs.mkdirSync(directory);
+  }
+  fs.writeFileSync(path.join(directory, fileName), JSON.stringify(content) );
 }
 
 const setFocus = function setFocus(project) {
@@ -32,10 +61,14 @@ const setFocus = function setFocus(project) {
 }
 app.on('ready', createWindow);
 
+process.on('exit', () => {
+  timer.stop = Date.now();
+    projects[0].addMilliSeconds(timer.stop - timer.start);
+    saveReport(projects);
+    console.log(`Timer ran for ${(projects[0].elapsedTime) / 1000} seconds.`);
+});
+
 app.on('window-all-closed', () => {
-    timer.stop = Date.now();
-    projects[currentProjectId].addMilliSeconds(timer.stop - timer.start);
-    console.log(`Timer ran for ${(projects[currentProjectId].elapsedTime) / 1000} seconds.`);
     if (process.platform !== 'darwin') {
     app.quit();
   }
--- project.js
+++ project.js
@@ -1,9 +1,14 @@
-module.exports = function Project(id, name, description) {
-    this.id = id;
-    this.name = name;
-    this.description = description;
-    this.elapsedTime = 0;
+module.exports = function Project(identifier, projectName, desc) {
+    const id = identifier;
+    let name = projectName;
+    let description = desc;
+    let elapsedTime = 0;
+    
     this.addMilliSeconds = function addMilliSeconds(millis) {
-        this.elapsedTime = this.elapsedTime + millis;
-    }
+        elapsedTime += millis;
+    };
+    
+    this.toCsvRecord = function toCsvRecord() {
+        return `${elapsedTime}; ${name}; ${description}`;
+    };
 };
