243d37f26e2368c332c7f24ea332ceb349fd1a9c
code303 <code303@example.com>
2019-12-06 00:11:48 +0100

    Fix to show only minutes of an hour

--- tools.js
+++ tools.js
@@ -1,7 +1,7 @@
 module.exports = {
     formatHHMM: function formatHHMM(millis) {
-        const mins = Math.floor(millis / 60000);
-        const hours = Math.floor( mins / 60);
+        const hours = Math.floor( millis / 3600000);
+        const mins = Math.floor((millis / 60000) - hours * 60);
         const mm = (mins < 10) ? '0' + mins : mins;
         const hh = (hours < 10) ? '0' + hours : hours;
         return `${hh}:${mm}`;
